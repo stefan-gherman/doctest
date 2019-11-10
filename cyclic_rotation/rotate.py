8b9a1609afc25f27f67662f962407605cd892ccb
Stefan Gherman <stefangheman@gmail.com>
2019-11-10 03:03:03 +0200

    Rewrite of main logic
    Added the abillity to shift left or right with numbers larger than the lenght of the string, true cyclic

--- cyclic_rotation/rotate.py
+++ cyclic_rotation/rotate.py
@@ -8,15 +8,41 @@ def cyclic_rotation(string,places):
     'ideT'
     >>> cyclic_rotation('Horse',-2)
     'rseHo'
+    >>> cyclic_rotation('Tide',5)
+    'eTid'
+    >>> cyclic_rotation('Horse',-7)
+    'rseHo'
+    >>> cyclic_rotation('Horse',-5)
+    'Horse'
+    >>> cyclic_rotation('Tide',7)
+    'ideT'
     '''
+
+    if abs(places) >= len(string):
+        new_places = abs(places) % len(string) 
+        ''' 
+        Based on some caculations it has occured that if a number larger than the lenght of the string 
+        is passed, the same number of rotations as the result of places%/string lenght will occur
+        
+        '''
+    else:
+        new_places = abs(places)    
+            
+    #print(places)
 
 
     cycle_string = ""
 
-    for i in range(len(string)):
-        if places > 0:
-            cycle_string += string[i-places]
-        else:
-            cycle_string += string[i+places-1]
+    if places > 0:
+        for i in range(len(string)):
+            cycle_string += string[i-new_places]
+    else:
+        new_string = string[-1::-1]
+        #print(new_string)
+        for i in range(len(new_string)):
+            cycle_string += new_string[i-new_places]
+        #print(i ,string[i],cycle_string)
+        cycle_string = cycle_string[-1::-1]
+
     return cycle_string
 
